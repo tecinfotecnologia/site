54809ebc11dd56d04befb644959c412dfd718f76
tecinfotecnologia <thalesalmeida.tecinfo@gmail.com>
2023-01-06 09:34:03 -0300

    ENVIADO PARA O REPOSITORIO DO SITE

---
+++ testphpmailer.php
@@ -0,0 +1,24 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->Username = 'thalesalmeida.tecinfo@gmail.com';
+$mail->Password = '358oeFak';
+$mail->setFrom('thalesalmeida.teinfo@gmail.com', 'Your Name');
+$mail->addReplyTo('thalesalmeida.tecinfo@gmail.com', 'Your Name');
+$mail->addAddress('thalesalmeida.tecinfo@gmail.com', 'Receiver Name');
+$mail->Subject = 'Testing PHPMailer';
+$mail->msgHTML(file_get_contents('message.html'), __DIR__);
+$mail->Body = 'This is a plain text message body';
+//$mail->addAttachment('test.txt');
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'The email message was sent.';
+}
+?>
